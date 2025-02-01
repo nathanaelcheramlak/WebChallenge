100d4031a4fdeff2770886e67ed9b40d3bb337a6
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2025-02-01 21:47:12 +0300

    rates

---
+++ multi-step-form-react/src/assets/rates.jsx
@@ -0,0 +1,23 @@
+export const service_yearly_rate = {
+  'online-service' : 10,
+  'larger-storage' : 20,
+  'custom-profile' : 20
+}
+
+export const service_monthly_rate = {
+  'online-service' : 1,
+  'larger-storage' : 2,
+  'custom-profile' : 2
+}
+
+export const plan_yearly_rate = {
+    arcade: 90,
+    advanced: 120,
+    pro: 150
+}
+
+export const plan_monthly_rate = {
+    arcade: 9,
+    advanced: 12,
+    pro: 15   
+}
